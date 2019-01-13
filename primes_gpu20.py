80dec883124f01af70fd6caf646faa364db3d61f
Natalia Chabros <natalach.mail@gmail.com>
2019-01-13 22:53:53 +0100

    20 threads

--- primes_gpu20.py
+++ primes_gpu20.py
@@ -13,8 +13,10 @@ whole_program = time.time()
 
 lower_bound = 1739834324567
 upper_bound = 1739834324667
-diff = upper_bound - lower_bound
+diff = (upper_bound - lower_bound)-1
+d = int((upper_bound - lower_bound)/20)
 
+d = numpy.int32(d)
 
 nums = list(range (lower_bound, upper_bound))
 nums = numpy.asarray(nums)
@@ -24,6 +26,7 @@ res = res.astype(numpy.int64)
 
 nums_gpu = cuda.mem_alloc(nums.nbytes)
 res_gpu = cuda.mem_alloc(res.nbytes)
+# d_gpu = cuda.mem_alloc(d.nbytes)
 
 # arr_gpu = cuda.mem_alloc(arr.nbytes)
 
@@ -32,50 +35,47 @@ cuda.memcpy_htod(res_gpu, res)
 
 
 mod = SourceModule("""
-  __global__ void primify (long long int *a, long long int *res)
+  __global__ void primify (long long int *a, long long int *res, int diff)
   {
-    long long int idx = threadIdx.x;
-    if(idx == 0) {
-            printf(" " );
-    }
-    long long int j = 0;
+    int idx = threadIdx.x;
+    int d = diff;
+    int k = 0;
+    int j = 0;
     long long int i = 0;
-    long long int sq = sqrtf(a[idx]);
-    for (i = 2; i<=sq; ++i){
-        long long int rem = remainder(a[idx], i);
+    long long int sq = 0;
+    int m = idx*5;
 
-        if(rem == 0){
-            j++;
-        }
-
-    }
-    if(j == 0){
-        int l = 0;
-        for(l; l<sq; ++l){
-            if(res[l] == 0){
-                res[l] = a[idx];
-                break;
+    for (k = 0; k<=d; ++k){
+        m = k+idx*5;
+        i = 0;
+        j = 0;
+        sq = sqrtf(a[m]);
+        for (i = 2; i<=sq; ++i){
+            int rem = remainder(a[m], i);
+            if(rem == 0){
+                j++;
             }
+
         }
+        if(j == 0){
+            res[m] = a[m];
+        }
+
     }
-
-
 
   }
   """)
 
 func = mod.get_function("primify")
 
-func( nums_gpu,res_gpu, block=(diff,1,1))
+func( nums_gpu,res_gpu, d, block=(20,1,1))
 res_fromgpu = numpy.empty_like(res)
 #
 cuda.memcpy_dtoh(res_fromgpu, res_gpu)
-for x in range(0,diff):
-    if res_fromgpu[x] == 0:
-        res_fromgpu = res_fromgpu[0:x]
-        break
 
-print (res_fromgpu)
+res = res_fromgpu[res_fromgpu != 0]
+print (res)
 program_time = time.time() - whole_program
+
 
 print ("total time:  % s seconds" % program_time)
